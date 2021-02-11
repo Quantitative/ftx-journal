776560896b354dbd626425b8e8b4a3a5bebca9d8
CosmonautC_Desktop <elliot.sjogren@protonmail.com>
2021-02-11 20:23:10 +0100

    app.py fixed and added environment variables

--- app.py
+++ app.py
@@ -1,31 +1,67 @@
 import hmac
 import requests
 import time
-from dateutil.parser import parse
 from client import FtxClient
+import psycopg2 as s3
+
+#For environment variables
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 
 class ftx_tracker():
 
     def __init__(self):
-        self.key                = "M5cZ4pgbCutRKT5UbLXQdmfxEpGAWt_P8a4d3u4W"
-        self.secret             = "2P8weUIPJiH8CTKg6FLdfiEmZTsfqd-udsNTkZTg"
+
+        #FTX API details
+        self.key                = os.getenv("KEY")
+        self.secret             = os.getenv("SECRET")
+
+        #Database details
+        self.db                 = os.getenv("db")
+        self.host               = os.getenv("host")
+        self.user               = os.getenv("user")
+        self.password           = os.getenv("password")
+        self.table_name         = "balance"
+
+        #Client/API connection
         self.client             = FtxClient(api_key=self.key, api_secret=self.secret)
-        self.base_url           = "https://ftx.com/api"
-        self.endpoint_account   = "/account"
+        
+        #Getting total usd and adding to database
+        self.create_table()
 
-        self.headers            = {}
-
-        self.get_account()
+        total_usd = self.get_account()
+        self.add_data(total_usd)
+        # self.show_data()
 
 
     def get_account(self):
         data = self.client.get_balances()
+        usd  = sum([balance["usdValue"] for balance in data])
+        
+        return usd
 
+    def create_table(self):
+        with s3.connect(host=self.host, database=self.db, user=self.user, password=self.password) as conn:
+            cursor = conn.cursor()
+            cursor.execute("CREATE TABLE IF NOT EXISTS {} (timestamp INTEGER, balance INTEGER)".format(self.table_name))
+            conn.commit()
 
-        print(data)
+    def add_data(self, usd):
+        with s3.connect(host=self.host, database=self.db, user=self.user, password=self.password) as conn:
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO {} VALUES({}, {})".format(self.table_name, int(time.time()), usd))
+            conn.commit()
+
+    # def show_data(self):
+    #     with s3.connect(host=self.host, database=self.db, user=self.user, password=self.password) as conn:
+    #         cursor = conn.cursor()
+    #         cursor.execute("SELECT * FROM {}".format(self.table_name))
+    #         print(cursor.fetchall())
+
 
 ftx_tracker()
-
-
-    
